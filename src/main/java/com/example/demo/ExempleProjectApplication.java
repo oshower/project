4227ce298698428165223aeb8697be0373081e74
oshow <oshow@192.168.200.197>
2019-07-13 15:35:29 +0900

    첫번쨰 커밋!

---
+++ src/main/java/com/example/demo/ExempleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExempleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExempleProjectApplication.class, args);
+	}
+
+}
